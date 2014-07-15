4dcdc40b1f7a72a72b9420229cc6aafed7baabcb
Hanwen (Steinway) Wu <steinwaywhw@gmail.com>
2014-07-15 11:52:13 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-/etc/init.d/tomcat6 start
+service tomcat7 start
 
 # link mounted source directory to opengrok
-ln -s /src /grok/src
+ln -s /src $OPENGROK_INSTANCE_BASE/src
 
 # first-time index
 echo "** Running first-time indexing"
@@ -12,7 +12,8 @@ cd /opengrok/bin
 # ... and we keep running the indexer to keep the container on
 echo "** Waiting for source updates..."
 touch $OPENGROK_INSTANCE_BASE/reindex
-inotifywait -m -e CLOSE_WRITE $OPENGROK_INSTANCE_BASE/reindex | while read f; do
+inotifywait -mr -e CLOSE_WRITE $OPENGROK_INSTANCE_BASE/src | while read f; do
+  printf "*** %s\n" "$f"
   echo "*** Updating index"
   ./OpenGrok index
 done
