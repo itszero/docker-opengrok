c04a8ddacc93d405ee31fce8ab88029d7c0aa4cc
Hanwen (Steinway) Wu <steinwaywhw@gmail.com>
2014-07-15 11:36:50 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ mkdir $OPENGROK_INSTANCE_BASE
 mkdir $OPENGROK_INSTANCE_BASE/data
 mkdir $OPENGROK_INSTANCE_BASE/etc
 
-wget -O - http://java.net/projects/opengrok/downloads/download/opengrok-0.11.1.tar.gz | tar zxvf -
+wget -O - https://java.net/projects/opengrok/downloads/download/opengrok-0.12.1.tar.gz | tar zxvf -
 mv opengrok-* opengrok
 cd /opengrok/bin
 ./OpenGrok deploy
