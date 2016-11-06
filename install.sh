b0e42c3ccbe094b86d336219503383a90d0fa0dd
Klavs Madsen <ktrance@gmail.com>
2016-11-06 11:24:11 +0100

    upgraded opengrok 0.12.1.6, base image to ubuntu 16.10 and openjdk to version 8 (#7)

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@ mkdir $OPENGROK_INSTANCE_BASE
 mkdir $OPENGROK_INSTANCE_BASE/data
 mkdir $OPENGROK_INSTANCE_BASE/etc
 
-wget -O - https://java.net/projects/opengrok/downloads/download/opengrok-0.12.1.tar.gz | tar zxvf -
+wget https://github.com/OpenGrok/OpenGrok/files/467358/opengrok-0.12.1.6.tar.gz.zip
+unzip -p opengrok-0.12.1.6.tar.gz.zip | tar zxvf -
+rm opengrok-0.12.1.6.tar.gz.zip
 mv opengrok-* opengrok
 cd /opengrok/bin
 ./OpenGrok deploy
